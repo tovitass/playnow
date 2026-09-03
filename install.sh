8c86eb283fc7de95e181bcca94944e36002429ef
tovitass <tovitas.gaming@gmail.com>
2026-09-03 12:35:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 echo "Installing playnow..."
 
-# Install binary to system path
-sudo cp playnow /usr/local/bin/playnow
+# Fetch the executable directly from GitHub into system path
+sudo curl -sSL https://raw.githubusercontent.com/tovitass/playnow/main/playnow -o /usr/local/bin/playnow
 sudo chmod +x /usr/local/bin/playnow
 
 echo "playnow installed successfully! Run 'playnow' in any terminal."
