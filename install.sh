f3ecc532f6848d247ad920c7b6e5f2382d2ed93a
tovitass <tovitas.gaming@gmail.com>
2026-09-03 12:43:15 +0200

    Update install.sh to fetch remote binary

--- install.sh
+++ install.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 echo "Installing playnow..."
-
-# Fetch binary directly from GitHub into /usr/local/bin
 sudo curl -sSL https://raw.githubusercontent.com/tovitass/playnow/main/playnow -o /usr/local/bin/playnow
 sudo chmod +x /usr/local/bin/playnow
-
 echo "playnow installed successfully! Run 'playnow' in any terminal."
